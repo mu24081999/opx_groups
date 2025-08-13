8f854550f9f5d3c6758a8a8bae8e04d058294c71
mu24081999 <mu24081999@gmail.com>
2025-08-13 13:22:51 -0700

    Add animated background elements and enhanced scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import ParticleRing from "./components/ParticleRing";
 import ParallaxLayout from "./Layout/ParallaxLayout";
+import AnimatedBackgroundElements from "./components/AnimatedBackgroundElements";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <ParallaxLayout/>
+      <ParallaxLayout />
+      <AnimatedBackgroundElements />
       {/* <ParticleRing /> */}
     </>
   );
