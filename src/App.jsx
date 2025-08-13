4d1df60fe529f52315fe2ccc4fd47b4852c6385a
mu24081999 <mu24081999@gmail.com>
2025-08-13 13:28:40 -0700

    Restore complete animated web app with enhanced features and 3D effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import { useState } from "react";
 import ParticleRing from "./components/ParticleRing";
 import ParallaxLayout from "./Layout/ParallaxLayout";
 import AnimatedBackgroundElements from "./components/AnimatedBackgroundElements";
+import EnhancedHeroSection from "./components/EnhancedHeroSection";
+import SmoothScrollNav from "./components/SmoothScrollNav";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <ParallaxLayout />
-      <AnimatedBackgroundElements />
+      <SmoothScrollNav />
+      
+      {/* Enhanced Hero Section */}
+      <section id="hero">
+        <EnhancedHeroSection />
+        <AnimatedBackgroundElements />
+      </section>
+
+      {/* Original Layout */}
+      <section id="about">
+        <ParallaxLayout />
+      </section>
+
       {/* <ParticleRing /> */}
     </>
   );
