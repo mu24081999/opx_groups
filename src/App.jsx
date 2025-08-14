3c1af27ab56f838d9d6f6a39d7728bb8d04b7bba
Muhammad Umar Liaqat <116753849+mu24081999@users.noreply.github.com>
2025-08-15 03:18:06 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useRef } from "react";
 
 import Preloader from "./components/PreLoader";
-import ParticleRingBackground from "./components/ParticleRingBackground";
+// import ParticleRingBackground from "./components/ParticleRingBackground";
 // import ScrollCube from "./components/ScrollCube";
 import Parallax3DLayout from "./components/Parallax3DLayout";
 import FloatingNavBar from "./components/Navbar";
-import LogoScroll from "./components/LogoScroll";
+// import LogoScroll from "./components/LogoScroll";
 
 const App = () => {
   const orbRef = useRef(null);
@@ -53,32 +53,32 @@ const App = () => {
   }
 
   // Language selection screen
-  if (!language) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-black text-[#888888] relative overflow-hidden">
-        <div className="flex space-x-10 relative z-10">
-          <button
-            ref={buttonRefs.en}
-            onClick={() => handleLanguageSelect("en")}
-            className="lang-btn group"
-          >
-            English
-          </button>
-          <button
-            ref={buttonRefs.ar}
-            onClick={() => handleLanguageSelect("ar")}
-            className="lang-btn group"
-          >
-            العربية
-          </button>
-        </div>
-        <div
-          ref={orbRef}
-          className="absolute w-6 h-6 rounded-full animate-orb-bounce z-0"
-        ></div>
-      </div>
-    );
-  }
+  // if (!language) {
+  //   return (
+  //     <div className="flex items-center justify-center h-screen bg-black text-[#888888] relative overflow-hidden">
+  //       <div className="flex space-x-10 relative z-10">
+  //         <button
+  //           ref={buttonRefs.en}
+  //           onClick={() => handleLanguageSelect("en")}
+  //           className="lang-btn group"
+  //         >
+  //           English
+  //         </button>
+  //         <button
+  //           ref={buttonRefs.ar}
+  //           onClick={() => handleLanguageSelect("ar")}
+  //           className="lang-btn group"
+  //         >
+  //           العربية
+  //         </button>
+  //       </div>
+  //       <div
+  //         ref={orbRef}
+  //         className="absolute w-6 h-6 rounded-full animate-orb-bounce z-0"
+  //       ></div>
+  //     </div>
+  //   );
+  // }
 
   // Main site
   return (
